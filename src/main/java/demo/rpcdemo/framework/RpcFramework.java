24d69bc432793f2f295453db50e98a810cac0e81
yuquan.wang <yuquan.wang@qunar.com>
2019-03-10 11:36:07 +0800

    m

--- src/main/java/demo/rpcdemo/framework/RpcFramework.java
+++ src/main/java/demo/rpcdemo/framework/RpcFramework.java
@@ -14,36 +14,35 @@ public class RpcFramework {
         for (; ; ) {
             try {
                 final Socket socket = server.accept();
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
+                new Thread(() -> {
+                    try {
                         try {
+                            ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
                             try {
-                                ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+                                // 1.获取方法名,参数类型,参数值(这个顺序是客户端调用服务时的传递顺序)
+                                String methodName = input.readUTF();
+                                Class<?>[] parameterTypes = (Class<?>[]) input.readObject();
+                                Object[] arguments = (Object[]) input.readObject();
+                                ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
                                 try {
-                                    // 获取方法名,参数类型,参数值(这个顺序是客户端调用服务时的传递顺序)
-                                    String methodName = input.readUTF();
-                                    Class<?>[] parameterTypes = (Class<?>[]) input.readObject();
-                                    Object[] arguments = (Object[]) input.readObject();
-                                    ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
-                                    try {
-                                        Method method = service.getClass().getMethod(methodName, parameterTypes);
-                                        Object result = method.invoke(service, arguments);
-                                        output.writeObject(result);
-                                    } catch (Throwable t) {
-                                        output.writeObject(t);
-                                    } finally {
-                                        output.close();
-                                    }
+                                    Method method = service.getClass().getMethod(methodName, parameterTypes);
+                                    // 2.利用反射调用指定服务的指定方法
+                                    Object result = method.invoke(service, arguments);
+                                    // 3.执行结果返回给客户端
+                                    output.writeObject(result);
+                                } catch (Throwable t) {
+                                    output.writeObject(t);
                                 } finally {
-                                    input.close();
+                                    output.close();
                                 }
                             } finally {
-                                socket.close();
+                                input.close();
                             }
-                        } catch (Exception e) {
-                            e.printStackTrace();
+                        } finally {
+                            socket.close();
                         }
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
                 }).start();
             } catch (Exception e) {
@@ -54,18 +53,21 @@ public class RpcFramework {
 
     @SuppressWarnings("unchecked")
     public static <T> T refer(final Class<T> interfaceClass, final String host, final int port) {
+        // 1.将本地的接口调用转换成JDK的动态代理，在动态代理中实现接口的远程调用
         return (T) Proxy.newProxyInstance(interfaceClass.getClassLoader(),
                 new Class<?>[]{interfaceClass}, new InvocationHandler() {
                     public Object invoke(Object proxy, Method method, Object[] arguments) throws Throwable {
+                        // 2.创建Socket客户端，根据指定地址连接远程服务提供者
                         Socket socket = new Socket(host, port);
                         try {
                             ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
                             try {
+                                // 3.将远程服务调用所需的接口类、方法名、参数列表等编码后发送给服务提供者
                                 output.writeUTF(method.getName());
                                 output.writeObject(method.getParameterTypes());
                                 output.writeObject(arguments);
-                                ObjectInputStream input = new ObjectInputStream(
-                                        socket.getInputStream());
+                                // 4.同步阻塞等待服务器返回应答，获取应答后返回
+                                ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
                                 try {
                                     Object result = input.readObject();
                                     if (result instanceof Throwable) {
